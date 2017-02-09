33bafb25b45e797b6d3bc7c255b9fa884a93e28e
Emanuele Disco <emawind84@gmail.com>
2017-02-09 19:50:44 +0900

    update

--- jenkins-slave/docker-entrypoint.sh
+++ jenkins-slave/docker-entrypoint.sh
@@ -7,16 +7,16 @@ PROXY_HOST=dev.sangah.com
 PROXY_USER=sangah
 SVN_HOST=125.141.221.126
 
-if [ "$AWS_S3_SVN_SSH_KEY" != "" ]; then
+if [ "$AWS_S3_PROXY_SSH_KEY" != "" ]; then
     AWS_DEFAULT_REGION=ap-northeast-2
     AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?missing}
     AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?missing}
-    AWS_S3_SVN_SSH_KEY=${AWS_S3_SVN_SSH_KEY:?missing}
+    AWS_S3_PROXY_SSH_KEY=${AWS_S3_PROXY_SSH_KEY:?missing}
 
     curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
     unzip awscli-bundle.zip
     ./awscli-bundle/install -b ~/bin/aws
-    ~/bin/aws s3 cp s3://${AWS_S3_SVN_SSH_KEY} /etc/ssl/private/$PROXY_SSH_KEY
+    ~/bin/aws s3 cp s3://${AWS_S3_PROXY_SSH_KEY} /etc/ssl/private/$PROXY_SSH_KEY
     chmod 0600 /etc/ssl/private/$PROXY_SSH_KEY
     ssh -f -N -i /etc/ssl/private/$PROXY_SSH_KEY -L 9001:${SVN_HOST}:80 ${PROXY_USER}@${PROXY_HOST}
 fi
